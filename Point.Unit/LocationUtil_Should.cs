6111c2a49b0f721a41d753b6fe7f9887413a3121
Hans Hedegaard Byager <hans-byager@live.dk>
2020-04-03 11:48:25 +0200

    Initial commit

---
+++ Point.Unit/LocationUtil_Should.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+using Xamarin.Essentials;
+
+namespace Point.Unit
+{
+    [TestClass]
+    public class LocationUtil_Should
+    {
+        [TestMethod]
+        public void Test_GetLocation()
+        {
+            
+        }
+
+    }
+}
---
+++ Point.Unit/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Point.Unit")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Point.Unit")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("d6221d94-d814-4f02-aaf4-46ad65e7be9e")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
